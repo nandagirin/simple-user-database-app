9981fcf327b119280f067f8ff7b5303d8984464b
nandagirin <nandagirin17@gmail.com>
2023-10-08 20:18:41 +0700

    Provide deployment pipeline of auth service

--- services/auth/tests/routes.test.js
+++ services/auth/tests/routes.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app.js')
+const app = require('../app.js');
 const ADMIN_PASS = process.env.ADMIN_PASS;
 
 describe('Auth endpoint', () => {
