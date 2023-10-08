2d7dad46801a5d0fd2a50012e6e19b3b9c7a47f4
nandagirin <nandagirin17@gmail.com>
2023-10-08 10:21:23 +0700

    Add auth service hc endpoint

--- services/auth/app.js
+++ services/auth/app.js
@@ -11,5 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/auth', router.authRouter);
+app.use('/healthz', router.healthCheckRouter);
 
 module.exports = app;
---
+++ services/auth/routes/healthcheck.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(_, res, next) {
+    return res.status(200).json({ message: "UP" });
+})
+
+module.exports = router;
