d650f1d87d186c4bde7ef0f1da694e486fae62c3
nandagirin <nandagirin17@gmail.com>
2023-10-10 14:59:55 +0700

    Add comment for token generation in auth service

--- services/auth/services/auth.js
+++ services/auth/services/auth.js
@@ -3,6 +3,15 @@ const jsonwebtoken = require('jsonwebtoken')
 const JWT_SECRET = process.env.JWT_SECRET
 const ADMIN_PASS = process.env.ADMIN_PASS
 
+/**
+ * This function handles token generation upon
+ * successful request from the client with valid
+ * credentials.
+ * @param {*} req HTTP Request
+ * @param {*} res HTTP Response
+ * @param {*} next Express middleware function
+ * @returns 
+ */
 const generateToken = (req, res, next) => {
   const { username, password } = req.body
   if (username === 'admin' && password === ADMIN_PASS) {
